7b4f69f20f44f7e9741b932fdb454a6ec7fc18f2
Szabolcs Antal <kamforka@hotmail.com>
2017-11-29 16:10:22 +0100

    Experiment with scheduling possibilities.

--- scheduler.py
+++ scheduler.py
@@ -1,24 +1,35 @@
+#!/usr/bin/python3
 """Scheduler for spiders."""
+import time
+
 import schedule
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+
+from takealot.spiders.deals import DealsSpider
 
 
 def crawl_job():
     """Job to start takealot spiders."""
-    process = CrawlerProcess(get_project_settings())
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    deferred = runner.crawl(DealsSpider)
+    deferred.addCallback(reactor.callLater, 5, crawl_job)
+    return deferred
 
-    # 'deals' is the name of one of the spider
-    process.crawl('deals')
-    process.start() # the script will block here until the crawling is finished
 
 def init_schedules():
     """Initialize schedules for jobs."""
-    schedule.every(30).seconds.do(crawl_job)
+    schedule.every(20).seconds.do(crawl_job)
 
 if __name__ == '__main__':
+    crawl_job()
 
-    init_schedules()
-    
-    while True:
-        schedule.run_pending()
+    reactor.run()
+
+    # init_schedules()
+
+    # start the twisted reactor
+    # while True:
+    #     schedule.run_pending()
