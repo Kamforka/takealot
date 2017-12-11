1f2facafab7fc4dd3b838216e418ee834b900901
Szabolcs Antal <kamforka@hotmail.com>
2017-12-11 15:53:50 +0100

    Start generalization of `DailyDealsPipeline`. Add `from_crawler` factory method. Add some file handling refactoring.

--- takealot/pipelines.py
+++ takealot/pipelines.py
@@ -24,16 +24,38 @@ class DefaultValuePipeline(object):
 
 
 # csv file name template string
-DAILY_DEALS_CSV_PATH_TEMPLATE = '/home/antalszabolcs01/{:%Y-%m-%d}_deals.csv'
+DAILY_DEALS_CSV_PATH_TEMPLATE = '{:%Y-%m-%d}_deals.csv'
+
+# hourly_fieldnames, last_scrape, export_path, export_fields(custom_settings),
+# start_hour
+
 
 class DailyDealsPipeline(object):
     """Pipe daily deals items to a specific CSV file."""
+    @classmethod
+    def from_crawler(cls, crawler):
+        spider = crawler.spider
+        export_path = crawler.settings.get('export_path', '')
+        last_scrape = crawler.settings.get('last', False)
+        start_hour = int(crawler.settings.get('start_hour', 7))
+        hourly_fieldnames = crawler.settings.get('hourly_fieldnames', '').split(',')
+        export_fields = spider.custom_settings['FEED_EXPORT_FIELDS']
+
+        for fieldname in hourly_fieldnames:
+            if not fieldname in export_fields:
+                raise ValueError('Hourly fieldname "{}" not found in the export fields of {}.'
+                                 .format(fieldname, spider.__name__))
+        return cls()
+
     def __init__(self):
         """Daily Deal Pipeline constructor."""
-        self.file = None
-        self.items = []
-        self.spider = None
-        self.writer = None
+        self.file = None  # csv export file
+        self.items = []  # list of export items
+        self.spider = None  # spider that utilizing the pipeline
+        self.writer = None  # csv dictwriter
+        self.hourly_scrape = False
+        self.last_scrape = False
+        self.export_path = ''
 
     def open_spider(self, spider):
         """Method to handle spider opening."""
@@ -44,6 +66,7 @@ class DailyDealsPipeline(object):
             # when a file already exists with the generated name
             # it means that an hourly scrapie is going on
             # so read in the whole csv and extend its headers
+            self.hourly_scrape = True
             with open(csv_path, 'r') as old_file:
                 old_items = csv.DictReader(old_file)
                 headers = old_items.fieldnames + self.create_ext_hourly_fieldnames()
@@ -55,18 +78,18 @@ class DailyDealsPipeline(object):
 
         self.file = open(csv_path, 'w')
         self.writer = csv.DictWriter(self.file, headers)
-        self.writer.writeheader()
 
     def close_spider(self, spider):
         """Method to handle spider closing."""
         if self.items:
+            self.writer.writeheader()
             self.writer.writerows(self.items)
 
         self.file.close()
 
     def process_item(self, item, spider):
         """Method to handle item processing."""
-        if self.items:
+        if self.hourly_scrape:
             # hourly scrape
             extended_hourly_fieldnames = self.create_ext_hourly_fieldnames()
             for new_item in self.items:
@@ -78,8 +101,14 @@ class DailyDealsPipeline(object):
 
         else:
             # initial scrape
-            self.writer.writerow(item)
+            self.items.append(item)
+            # self.writer.writerow(item)
         return item
+
+    def create_csv_path(self):
+        """Create CSV path string."""
+        return os.path.join(self.export_path,
+                            DAILY_DEALS_CSV_PATH_TEMPLATE.format(datetime.now()))
 
     def extend_item_fields(self, items):
         """Extend items with new fields for the hourly scrape."""
