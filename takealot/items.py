ad9ced1305c6448c525adf448cfc9a78e1c57ae5
Szabolcs Antal <kamforka@hotmail.com>
2017-12-11 15:54:18 +0100

    Extend `TakealotItem` class with input-output processors.

--- takealot/items.py
+++ takealot/items.py
@@ -1,24 +1,38 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+"""Takealot item models."""
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
 
+MOBILE_ITEM_URL_TEMPLATE = 'https://m.takealot.com/#product?id={id}'
+SELLER_URL_TEMPLATE = 'https://www.takealot.com/seller-profile.php?sellers={id}'
+
+def seller_url_processor(seller_id):
+    """Input processor for `seller_url`."""
+    return SELLER_URL_TEMPLATE.format(id=seller_id) if seller_id else None
+
+def mobile_url_processor(prod_id):
+    """Input processor for `mobile_url`."""
+    return MOBILE_ITEM_URL_TEMPLATE.format(id=prod_id)
+
+def price_processor(price):
+    """Input processor for product price."""
+    return price / 100.
 
 class TakealotItem(scrapy.Item):
     """Takealot item definition."""
-    # define the fields for your item here like:
-    id = scrapy.Field()
+    id = scrapy.Field(output_processor=TakeFirst())
     date = scrapy.Field()
-    product_name = scrapy.Field()
+    product_name = scrapy.Field(output_processor=TakeFirst())
     product_category = scrapy.Field()
-    url_desktop = scrapy.Field()
-    url_mobile = scrapy.Field()
-    seller_name = scrapy.Field()
-    seller_url = scrapy.Field()
-    price_normal = scrapy.Field()
-    price_offer = scrapy.Field()
-    stock_remaining = scrapy.Field()
+    url_desktop = scrapy.Field(output_processor=TakeFirst())
+    url_mobile = scrapy.Field(input_processor=MapCompose(mobile_url_processor),
+                              output_processor=TakeFirst())
+    seller_name = scrapy.Field(output_processor=TakeFirst())
+    seller_url = scrapy.Field(input_processor=MapCompose(seller_url_processor),
+                              output_processor=TakeFirst())
+    price_normal = scrapy.Field(input_processor=MapCompose(price_processor),
+                                output_processor=TakeFirst())
+    price_offer = scrapy.Field(input_processor=MapCompose(price_processor),
+                               output_processor=TakeFirst())
+    stock_remaining = scrapy.Field(output_processor=TakeFirst())
     warehouses = scrapy.Field()
