116ac1782d845c98426d8a08c262ffecaa67cee9
Szabolcs Antal <kamforka@hotmail.com>
2017-12-11 15:48:41 +0100

    Refactor daily deals scraper. Replace parent class `scrapy.Spider` to `SpiderBase`. Remove `parse_item`, as `SpiderBase` has the general implementation for it. Remove unnecessary imports and constants.

--- takealot/spiders/deals.py
+++ takealot/spiders/deals.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
 """Takealot daily deals item scraper."""
-import datetime as dt
-import json
 import requests
 
-import scrapy
-
-from takealot.items import TakealotItem
+from takealot.spiders import API_PRODUCT_URL, SpiderBase
 
 API_DAILY_DEALS_URL = ('https://api.takealot.com/rest/v-1-5-2/productlines/search'
                        '?rows={rows}&start={offset}&backend=arj-fbye-zz-fla-fcenax'
                        '&filter=Promotions:{id}&sort=ReleaseDate%20Descending'
                        '&detail=mlisting&filter=Available:true')
 
-API_PRODUCT_URL = 'https://api.takealot.com/rest/v-1-5-2/productline/{id}'
-
-MOBILE_ITEM_URL_TEMPLATE = 'https://m.takealot.com/#product?id={id}'
-
-SELLER_URL_TEMPLATE = 'https://www.takealot.com/seller-profile.php?sellers={id}'
 
 def get_daily_id():
     """Retrieve daily deal id from takealot api."""
@@ -28,14 +19,15 @@ def get_daily_id():
             return resp['id']
 
 
-class DealsSpider(scrapy.Spider):
+class DealsSpider(SpiderBase):
     """Spider to crawl daily deals from takealot.com/deals"""
     name = 'deals'
     allowed_domains = ['api.takealot.com']
     custom_settings = {
         'FEED_EXPORT_FIELDS': ['id', 'product_name'],
         'ITEM_PIPELINES': {
-            'takealot.pipelines.DailyDealsPipeline': 300,
+            'takealot.pipelines.DefaultValuePipeline': 300,
+            'takealot.pipelines.DailyDealsPipeline': 301,
         }
     }
 
@@ -82,33 +74,3 @@ class DealsSpider(scrapy.Spider):
             yield response.follow(API_DAILY_DEALS_URL.format(rows=self.rows,
                                                              offset=offset,
                                                              id=self.daily_id))
-
-    def parse_item(self, response):
-        """Parse additional properties from product page."""
-        jsonresponse = self.get_jsonresponse(response)
-        product = jsonresponse['response']
-
-        item = TakealotItem()
-        item['seller_name'] = product['skus'][0]['seller_name']#response.meta['seller_name']
-        if item['seller_name']:
-            item['seller_url'] = SELLER_URL_TEMPLATE.format(id=product['skus'][0]['seller_id'])
-
-        item['id'] = product['id']
-        item['date'] = '{:%Y-%m-%d}'.format(dt.datetime.now())
-        item['product_name'] = product['title']
-        item['url_desktop'] = product['uri']
-        item['url_mobile'] = MOBILE_ITEM_URL_TEMPLATE.format(id=product['id'])
-        item['price_normal'] = product['old_selling_price'] / 100.
-        item['price_offer'] = product['selling_price'] / 100.
-        item['stock_remaining'] = product['stock_on_hand']
-        item['warehouses'] = product['shipping_information']['stock_warehouses']
-
-        categories = product['categories']
-        item['product_category'] = [category['name'] for category in categories]
-
-        yield item
-
-    @staticmethod
-    def get_jsonresponse(response, encoding='utf-8'):
-        """Retrieve body dictionary from `Response` object."""
-        return json.loads(response.body.decode(encoding))
